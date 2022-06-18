d8a8bd866240d1cf9a88df889eeb3b8864c4ce8a
github_aliyukabir1 <aliyukabirlawal@gmail.com>
2022-06-18 15:55:29 +0100

    refactor

--- lib/features/presentation/bloc/quiz_bloc.dart
+++ lib/features/presentation/bloc/quiz_bloc.dart
@@ -13,27 +13,40 @@ class QuizBloc extends Bloc<QuizEvent, QuizState> {
   QuizBloc({required this.getQuiz}) : super(Initial()) {
     on<QuizEvent>((event, emit) async {
       if (event is GetQuizEvent) {
-        emit(Loading());
-        final failurOrList = await getQuiz.call();
-        failurOrList.fold(
-            (failure) => emit(const Error(message: 'can not find any quiz')),
-            (listOfQuiz) {
-          listOfQuizes = listOfQuiz;
-          emit(Loaded(quiz: listOfQuiz[indexOfQuiz]));
-        });
+        _getQuestions(emit);
       } else if (event is Next) {
-        if (indexOfQuiz < listOfQuizes.length) {
-          emit(Loading());
-          indexOfQuiz += 1;
-          emit(Loaded(quiz: listOfQuizes[indexOfQuiz]));
-        }
+        _nextQuestion(emit);
       } else if (event is Previous) {
-        if (indexOfQuiz > 0) {
-          emit(Loading());
-          indexOfQuiz -= 1;
-          emit(Loaded(quiz: listOfQuizes[indexOfQuiz]));
-        }
+        _previousQuestion(emit);
       }
     });
+  }
+
+  //methods for getting questions, next and previous questions...s
+  void _getQuestions(Emitter<QuizState> emit) async {
+    emit(Loading());
+    final failurOrList = await getQuiz.call();
+    failurOrList
+        .fold((failure) => emit(const Error(message: 'can not find any quiz')),
+            (listOfQuiz) {
+      listOfQuizes = listOfQuiz;
+      emit(Loaded(quiz: listOfQuiz[indexOfQuiz]));
+    });
+  }
+
+  void _previousQuestion(Emitter<QuizState> emit) {
+    if (indexOfQuiz > 0) {
+      emit(Loading());
+      indexOfQuiz -= 1;
+      emit(Loaded(quiz: listOfQuizes[indexOfQuiz]));
+    }
+  }
+
+  void _nextQuestion(Emitter<QuizState> emit) {
+    if (indexOfQuiz < listOfQuizes.length) {
+      emit(Loading());
+      indexOfQuiz += 1;
+      emit(Loaded(quiz: listOfQuizes[indexOfQuiz]));
+    }
   }
 }
